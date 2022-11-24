7a2d9f43623709986237e6af0fd7e14fa73fcb0d
EIIinor <ellinor.ljus@utb.ecutbildning.se>
2022-11-24 13:20:18 +0100

    api

--- src/contexts/productContext.tsx
+++ src/contexts/productContext.tsx
@@ -5,9 +5,9 @@ export interface IProductContext {
     product: Product
     setProduct: React.Dispatch<React.SetStateAction<Product>>
     products: Product[]
-    create: (e: React.FC) => void
+    create: (e: React.FormEvent) => void
     read: (id: number) => void
-    update: (id: number, e:React.FC) => void
+    update: (id: number, e:React.FormEvent) => void
     remove: (id: number) => void
 }
 
@@ -21,20 +21,24 @@ interface IProductProviderProps {
 const ProductProvider = ( {children} : IProductProviderProps ) => {
 
     const baseUrl = 'http://localhost:5000/api/products'
-    const defaultValues: Product = {articleNumber: '', name: '', price: 0, category: '', imageName: '', quantity: 0,  }
-    const [product, setProduct] = useState<Product>(defaultValues)
+
+    const product_default: Product = {articleNumber: '', name: '', price: 0, category: '', imageUrl: '', quantity: 0,  }
+
+    const [product, setProduct] = useState<Product>(product_default)
     const [products, setProducts] = useState<Product[]>([])
 
-    const create = async (e: React.FC) => {
+    const create = async (e: React.FormEvent) => {
+      e.preventDefault()
+
       const result = await fetch(`${baseUrl}`, {
         method: 'post',
         headers: {
           'Content-Type': 'application/json'
         },
-        body: JSON.stringify(products)
+        body: JSON.stringify(product)
       })
       if (result.status === 201)
-        setProducts(await result.json())
+        setProduct(product_default)
     }
 
     const read = async (id: number) => {
@@ -44,17 +48,18 @@ const ProductProvider = ( {children} : IProductProviderProps ) => {
     }
 
   
-    const update = async (id: number, e: React.FC) => {
+    const update = async (id: number, e: React.FormEvent) => {
+      e.preventDefault()
+
       const result = await fetch(`${baseUrl}/${id}`, {
-        method: 'post',
+        method: 'put',
         headers: {
           'Content-Type': 'application/json'
         },
         body: JSON.stringify(product)
       })
-      if (result.status === 200)
+      if (result.status === 200) 
         setProduct(await result.json())
-
     }
 
     const remove = async (id: number) => {
--- src/models/productsModel.ts
+++ src/models/productsModel.ts
@@ -3,6 +3,6 @@ export interface Product {
     name: string
     price: number
     category: string
-    imageName: string
+    imageUrl: string
     quantity: number
 }
